568fcd7eb47bf60f0395da9058f5a2817cbacc95
MBrouns <matthijs.brouns@gmail.com>
2021-12-12 20:38:16 +0100

    add linting + testing CI steps

--- src/skprometheus/pipeline.py
+++ src/skprometheus/pipeline.py
@@ -16,7 +16,7 @@ def _final_estimator_has(attr):
     return check
 
 
-DEFAULT_LATENCY_BUCKETS = (0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, float('inf'))
+DEFAULT_LATENCY_BUCKETS = (0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1., 2.5, 5., 7.5, 10., float('inf'))
 
 
 class Pipeline(pipeline.Pipeline):
