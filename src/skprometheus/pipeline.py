27777438c1d3f7848cd5fc12a0a8692a78821a8a
LeenmacXccl <leenmacxccl@admins-MacBook-Pro.local>
2021-12-14 17:43:10 +0100

    Added histogram for predict probas and metric extraction code in predict method. works, but not tested very well yet

--- src/skprometheus/pipeline.py
+++ src/skprometheus/pipeline.py
@@ -16,7 +16,22 @@ def _final_estimator_has(attr):
     return check
 
 
+def make_pipeline(*steps, memory=None, verbose=False):
+    """
+    Construct a :class:`Pipeline` from the given estimators.
+
+    This is a shorthand for the :class:`Pipeline` constructor; it does not
+    require, and does not permit, naming the estimators. Instead, their names
+    will be set to the lowercase of their types automatically.
+    """
+    return Pipeline(pipeline._name_estimators(steps), memory=memory, verbose=verbose)
+
+
 DEFAULT_LATENCY_BUCKETS = (0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1., 2.5, 5., 7.5, 10., float('inf'))
+DEFAULT_PROBA_BUCKETS = (0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+
+#label --> class
+#class_names == classes_ op steps[-1][1].classes_
 
 
 class Pipeline(pipeline.Pipeline):
@@ -31,19 +46,36 @@ class Pipeline(pipeline.Pipeline):
         verbose=False,
         *,
         prom_labels=None,
-        latency_buckets=DEFAULT_LATENCY_BUCKETS
+        latency_buckets=DEFAULT_LATENCY_BUCKETS,
+        proba_buckets=DEFAULT_PROBA_BUCKETS,
     ):
         self.prom_labels = prom_labels
         self.latency_buckets = latency_buckets
+        self.proba_buckets = proba_buckets
         self._model_predict_latency = Histogram(
             "model_predict_latency_seconds",
             "Time in seconds it takes to call `predict` on the model",
             labelnames=tuple(self.prom_labels.keys()) if self.prom_labels is not None else (),
             buckets=latency_buckets
         )
+        self._model_predict_proba = Histogram(
+            "model_predict_proba",
+            "Prediction probability for each class of the model",
+            labelnames=(tuple(self.prom_labels.keys()) if self.prom_labels is not None else ()) + ("class_",),
+            buckets=proba_buckets
+        )
         super().__init__(steps=steps, memory=memory, verbose=verbose)
 
     @available_if(_final_estimator_has("predict"))
     def predict(self, X, **predict_params):
-        with self._model_predict_latency.time():
-            return super().predict(X, **predict_params)
+        if hasattr(self.steps[-1][1], "predict_proba"):
+            predict_probas = super().predict_proba(X, **predict_params) #**predict_parms not applicable here?
+            classes = self.steps[-1][1].classes_
+            for idx, class_ in enumerate(classes):
+                for proba in predict_probas: #TODO extract this to utils func
+                    self._model_predict_proba.labels(class_=class_).observe(proba[idx])
+            with self._model_predict_latency.time():
+                return super().predict(X, **predict_params)
+        else:
+            with self._model_predict_latency.time():  #.labels(**self.prom_labels) werkt niet
+                return super().predict(X, **predict_params)
