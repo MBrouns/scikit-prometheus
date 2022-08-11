84a6c43df08c6fec8c4a15112beb4e2932ab2b67
Patricia Reina <preina@xccelerated.com>
2022-08-11 15:06:22 +0200

    ordinal enc and utils refactor

--- src/skprometheus/preprocessing.py
+++ src/skprometheus/preprocessing.py
@@ -1,8 +1,23 @@
+import numpy as np
 from functools import wraps
 
 from sklearn import preprocessing
 from skprometheus.metrics import MetricRegistry
 from skprometheus.utils import get_feature_names
+
+
+def feature_category_count(X, categories):
+
+    features = get_feature_names(X)
+
+    for idx, row in enumerate(categories.T):
+        for category in row:
+            if category is None:
+                category = "missing"
+                MetricRegistry.model_categorical(feature=str(features[idx]), category=category).inc()
+            MetricRegistry.model_categorical(feature=str(features[idx]), category=str(category)).inc()
+            
+
 
 
 class OneHotEncoder(preprocessing.OneHotEncoder):
@@ -24,16 +39,11 @@ class OneHotEncoder(preprocessing.OneHotEncoder):
         metric registry.
         """
         transformed_X = super().transform(X)
-        features = get_feature_names(X)
 
         # Use inverse method on transformed_X to get all missing values back as 'None'
         categories = self.inverse_transform(transformed_X)
 
-        for idx, row in enumerate(categories.T):
-            for category in row:
-                if not category:
-                    category = "missing"
-                MetricRegistry.model_categorical(feature=str(features[idx]), category=str(category)).inc()
+        feature_category_count(X, categories)
 
         return transformed_X
 
@@ -42,7 +52,7 @@ class OrdinalEncoder(preprocessing.OrdinalEncoder):
     """
     OrdinalEncoder that adds metrics to the prometheus metric registry.
     """
-    @wraps(preprocessing.OneHotEncoder.__init__, assigned=["__signature__"])
+    @wraps(preprocessing.OrdinalEncoder.__init__, assigned=["__signature__"])
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         MetricRegistry.add_counter(
@@ -57,15 +67,10 @@ class OrdinalEncoder(preprocessing.OrdinalEncoder):
         metric registry.
         """
         transformed_X = super().transform(X)
-        features = get_feature_names(X)
 
         # Use inverse method on transformed_X to get all missing values back as 'None'
         categories = self.inverse_transform(transformed_X)
 
-        for idx, row in enumerate(categories.T):
-            for category in row:
-                if not category:
-                    category = "missing"
-                MetricRegistry.model_categorical(feature=str(features[idx]), category=str(category)).inc()
+        feature_category_count(X, categories)
 
         return transformed_X
