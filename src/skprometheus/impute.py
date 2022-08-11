be7983343277a73cc5e1835c19b494b9d7130b83
Andrey Alekseev <andreyalekseev@admins-MacBook-Pro.local>
2022-08-11 17:00:07 +0200

    add metaclass

--- src/skprometheus/impute.py
+++ src/skprometheus/impute.py
@@ -2,6 +2,7 @@ from functools import wraps
 
 import numpy as np
 from sklearn import impute
+from sklearn.base import TransformerMixin
 
 from skprometheus.metrics import MetricRegistry
 from skprometheus.utils import get_feature_names
@@ -34,6 +35,17 @@ class SimpleImputer(impute.SimpleImputer):
         return super().transform(X)
 
 
+# class IterativeImputer(impute.IterativeImputer):
+#     @wraps(impute.IterativeImputer.__init__, assigned=["__signature__"])
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         MetricRegistry.add_counter("imputed", "the number of values imputed", additional_labels=("method", "feature"))
+#
+#     def transform(self, X):
+#         register_imputer_metrics(X, method="IterativeImputer")
+#         return super().transform(X)
+
+
 class MissingIndicator(impute.MissingIndicator):
     @wraps(impute.MissingIndicator.__init__, assigned=["__signature__"])
     def __init__(self, *args, **kwargs):
@@ -54,3 +66,38 @@ class KNNImputer(impute.KNNImputer):
     def transform(self, X):
         register_imputer_metrics(X, method="KNNImputer")
         return super().transform(X)
+
+
+class ImputerCreated(type):
+    def __new__(mcs, name, bases, d):
+        class_obj = super().__new__(mcs, name, bases, d)
+
+        # define __init__
+        setattr(class_obj, '__init__', ImputerCreated.init(class_obj, bases[0]))
+        setattr(class_obj, 'transform', ImputerCreated.transform(bases[0]))
+
+        return class_obj
+
+    @staticmethod
+    def init(class_obj, base):
+        @wraps(base.__init__, assigned=["__signature__"])
+        def new_init(self, *args, **kwargs):
+            super(class_obj, self).__init__(*args, **kwargs)
+            MetricRegistry.add_counter("imputed", "the number of values imputed",
+                                       additional_labels=("method", "feature"))
+
+        return new_init
+
+    @staticmethod
+    def transform(base):
+        def new_transform(self, X):
+            register_imputer_metrics(X, method=base.__name__)
+            return super().transform(X)
+
+        return new_transform
+
+
+KNNImputer = ImputerCreated("KNNImputer", (impute.KNNImputer,), {})
+
+
+
