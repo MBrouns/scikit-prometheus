6a382835be1d88980d6e6b08b1603eb197d2d37b
ambuj <pawar.ambuj@gmail.com>
2022-08-11 16:58:15 +0200

    Add a new dunder method for imputer to enable populating metrics when doing pickle.loads

--- src/skprometheus/impute.py
+++ src/skprometheus/impute.py
@@ -8,10 +8,13 @@ from skprometheus.utils import get_feature_names
 
 
 class SimpleImputer(impute.SimpleImputer):
+    def __new__(cls, *args, **kwargs):
+        MetricRegistry.add_counter("imputed", "the number of values imputed", additional_labels=("method", "feature"))
+        return super(SimpleImputer, cls).__new__(cls)
+
     @wraps(impute.SimpleImputer.__init__, assigned=["__signature__"])
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        MetricRegistry.add_counter("imputed", "the number of values imputed", additional_labels=("method", "feature"))
 
     def transform(self, X):
         features = get_feature_names(X)
