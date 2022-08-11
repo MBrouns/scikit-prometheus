d53a37af8a1757ec6875889c236c266e87988665
Patricia Reina <preina@xccelerated.com>
2022-08-11 11:59:41 +0200

    initial ordinal encoder

--- src/skprometheus/preprocessing.py
+++ src/skprometheus/preprocessing.py
@@ -36,3 +36,36 @@ class OneHotEncoder(preprocessing.OneHotEncoder):
                 MetricRegistry.model_categorical(feature=str(features[idx]), category=str(category)).inc()
 
         return transformed_X
+
+
+class OrdinalEncoder(preprocessing.OrdinalEncoder):
+    """
+    OrdinalEncoder that adds metrics to the prometheus metric registry.
+    """
+    @wraps(preprocessing.OneHotEncoder.__init__, assigned=["__signature__"])
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        MetricRegistry.add_counter(
+            "model_categorical",
+            "Counts category occurrence for each categorical feature.",
+            additional_labels=("feature", "category"),
+        )
+
+    def transform(self, X):
+        """
+        Transform method that adds the count for each category in each feature to the prometheus
+        metric registry.
+        """
+        transformed_X = super().transform(X)
+        features = get_feature_names(X)
+
+        # Use inverse method on transformed_X to get all missing values back as 'None'
+        categories = self.inverse_transform(transformed_X)
+
+        for idx, row in enumerate(categories.T):
+            for category in row:
+                if not category:
+                    category = "missing"
+                MetricRegistry.model_categorical(feature=str(features[idx]), category=str(category)).inc()
+
+        return transformed_X
